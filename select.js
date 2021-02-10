ca455e166df4fc01397da17c293e3752e7958f04
wafaa0509 <wafaa.boudaour@etu.univ-lyon1.fr>
2021-02-10 23:16:15 +0100

    change 1

--- select.js
+++ select.js
@@ -5,7 +5,7 @@ function showCity() {
     let villes = document.getElementsByClassName("ville");
 
     for (i=0; i<4; i++){
-      if(villes[i].id == nomVilleSelectionnee {
+      if(villes[i].id == nomVilleSelectionnee){
         villes[i].style.display = "inline";
       } else {
         villes[i].style.display = "none";
