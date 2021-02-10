c596e5911e57486f50502b1d23881eb899bf9a95
wafaa0509 <wafaa.boudaour@etu.univ-lyon1.fr>
2021-02-10 23:30:25 +0100

    Update select.js

--- select.js
+++ select.js
@@ -1,14 +1,13 @@
 function showCity() { 
-    console.log(ville);*/
-    var ville = document.getElementByld("ville").value; }*/
+    
     let nomVilleSelectionnee = document.getElementById("ville-select").value;
-    let villes = document.getElementsByClassName("ville");
+    let ville = document.getElementsByClassName("ville");
 
     for (i=0; i<4; i++){
-      if(villes[i].id == nomVilleSelectionnee){
-        villes[i].style.display = "inline";
+      if(ville[i].id == nomVilleSelectionnee){
+        ville[i].style.display = "inline";
       } else {
-        villes[i].style.display = "none";
+        ville[i].style.display = "none";
       }
     }
 }
